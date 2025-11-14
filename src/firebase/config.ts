b709a4718c06352f7504de25740472a9c27287a8
Gilbert Alves <gilbert@aptaesocial.com.br>
2025-11-14 11:27:07 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-2597869163-7e20f",
+  "appId": "1:1052502607014:web:4b7aae71442fafb143e067",
+  "apiKey": "AIzaSyA-YA5kwY0bB6ivMWgMcs-oKxk0JHK5h1A",
+  "authDomain": "studio-2597869163-7e20f.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "1052502607014"
+};
