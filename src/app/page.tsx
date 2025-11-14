d31f489b194e69920ba984e394424a19c044c90f
Gilbert Alves <gilbert@aptaesocial.com.br>
2025-11-14 11:52:37 +0000

    Qual a url "/" e o que ela deve abrir? Está abrindo o gerenciamento de e

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,11 @@
 'use client';
 
-import { redirect } from 'next/navigation';
-import { useEffect } from 'react';
+import MainDashboard from "@/components/dashboard/main-dashboard";
 
 export default function Home() {
-  useEffect(() => {
-    redirect('/admin/companies');
-  }, []);
-
-  return null;
+  return (
+    <div className="flex-1 space-y-4 p-4 sm:p-8 pt-6">
+      <MainDashboard />
+    </div>
+  );
 }
