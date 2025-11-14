a99f4dd12892ad23f22b1d81934f5afed64dfc97
Gilbert Alves <gilbert@aptaesocial.com.br>
2025-11-14 18:14:15 +0000

    Posso remover este acesso de e-mail e senha?

--- src/components/layout/header.tsx
+++ src/components/layout/header.tsx
@@ -34,9 +34,9 @@ export default function AppHeader() {
         <DropdownMenuContent className="w-56" align="end" forceMount>
           <DropdownMenuLabel className="font-normal">
             <div className="flex flex-col space-y-1">
-              <p className="text-sm font-medium leading-none">RH Admin</p>
+              <p className="text-sm font-medium leading-none">Admin</p>
               <p className="text-xs leading-none text-muted-foreground">
-                rh@cliente.com
+                Usuário Anônimo
               </p>
             </div>
           </DropdownMenuLabel>
